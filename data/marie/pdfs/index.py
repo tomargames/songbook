08e2edbcb35a50ecd65a79315bc30bb7aefca65a
tomargames <tomargames@gmail.com>
2022-10-02 17:57:03 -0400

    first pass at data

---
+++ data/marie/pdfs/index.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Created on 11/25/2020 by marie
+generic index.py to put in folders that should allow no direct access
+#!/usr/bin/python3
+#!C:\ProgramData\Anaconda3\python.exe#!/Users/tomar/Anaconda3/python.exe
+@author: marie
+"""
+print("Content-type: text/html \n")
+print("marie says you can't access this folder")
